674ee5e0d9d0ec0923729d4e1c755ef211f2e2a4
luongthomas <luongt@oregonstate.edu>
2017-02-06 18:06:17 +0700

    Changed folder name for consistency of coinEstimator folder

--- Magic 8 Ball/magic8ball.py
+++ Magic 8 Ball/magic8ball.py
@@ -15,26 +15,35 @@ from time import sleep
 from easygui import *
 import sys
 
+def showFortune():
+	responses = ["Yes I believe so", "No I don't think so", "Anything is possible", "If you truely believe so, it will become true",
+		"Only on Tuesdays", "Please ask later when I feel like answering", "No",
+		"Yes", "What would your mother say?", "Please consult the Magic 9 Ball about this matter, I'm not qualified enough",
+		"Does a turtle have a shell?", "Does an ant have muscles?", "I think so",
+		"Yeah, I'm sure of it", "No, it will never happen", "Reconsider your life choices",
+		"Do I look like a miracle worker?", "Impossible", "Never say never - 'Justin Beiber'",
+		"I think Google is better qualified on this matter"]
+
+	indexResponse = randint(0,19)
+	fortune = "The Magic 8 Ball says: " + responses[indexResponse]
+
+	msgbox("Thinking...")
+	sleep(2)
+	msgbox(fortune)
+
+def displayPrompt()
+
 def magicEightBall():
 	msg = "State your question to the Magic 8 Ball:  "
 
 	choices = ["Ask", "Clear", "Play Again", "Quit"]
 	reply = buttonbox(msg, choices=choices)
 
-	responses = ["Yes I believe so", "No I don't think so", "Anything is possible", "If you truely believe so, it will become true",
-	"Only on Tuesdays", "Please ask later when I feel like answering", "No",
-	"Yes", "What would your mother say?", "Please consult the Magic 9 Ball about this matter, I'm not qualified enough",
-	"Does a turtle have a shell?", "Does an ant have muscles?", "I think so",
-	"Yeah, I'm sure of it", "No, it will never happen", "Reconsider your life choices",
-	"Do I look like a miracle worker?", "Impossible", "Never say never - 'Justin Beiber'",
-	"I think Google is better qualified on this matter"]
-	indexResponse = randint(0,19)
+	if (reply == "Ask"):
+		
 
-	fortune = "The Magic 8 Ball says: " + responses[indexResponse]
+	elif (reply == "Clear"):
 
-	msgbox("Thinking...")
-	sleep(2)
-	msgbox(fortune)
 
 magicEightBall()
 
